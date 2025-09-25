b2c8406e58665692ad2e430017c15b396f0ae023
Bhuvanesh Jagtap <deshmukhkanha25@gmail.com>
2025-09-26 02:44:18 +0530

    Color Changer

--- Project1/script1.js
+++ Project1/script1.js
@@ -1,5 +1,6 @@
 const buttons = document.querySelectorAll(".boxes");
 const body = document.body;
+
 buttons.forEach((button) => {
     button.addEventListener("click", (e) => {
         const bgc = body.style.backgroundColor;
